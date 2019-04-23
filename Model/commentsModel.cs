bcf16caf010c016c4cf99f0fbbbbac92566ab823
Vitor <vcgrossi@gmail.com>
2019-04-23 20:13:44 -0300

    ;

--- Model/commentsModel.cs
+++ Model/commentsModel.cs
@@ -3,7 +3,6 @@ namespace WpBlog.model
 {
     public class comments{
         public int id { get; set; }
-    
         public user user{ get; set; }
         public DateTime createDate{ get; set; }
         public string content{ get; set; }
